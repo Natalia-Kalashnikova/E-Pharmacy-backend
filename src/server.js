373ea020385d983dacfc563524595a6d67d06b9c
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-01-15 01:45:25 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(
     cors({
-      origin: 'https://e-pharmacy-client-five.vercel.app',
+      origin: ['http://localhost:5173', 'https://e-pharmacy-client-five.vercel.app'],
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       allowedHeaders: ['Content-Type', 'Authorization'],
       credentials: true,
