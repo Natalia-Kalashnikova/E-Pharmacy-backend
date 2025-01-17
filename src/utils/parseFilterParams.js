39615ac2218d7d7d4aca834a3bb2077e583c8a88
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-01-18 00:19:45 +0200

    Update parseFilterParams.js

--- src/utils/parseFilterParams.js
+++ src/utils/parseFilterParams.js
@@ -1,6 +1,17 @@
+// export const parseFilterParams = (query) => {
+//   const filter = {};
+//   if (query.category) {
+//     filter.category = query.category;
+//   }
+//   if (query.name) {
+//     filter.name = query.name;
+//   }
+//   return filter;
+// };
+
 export const parseFilterParams = (query) => {
   const filter = {};
-  if (query.category) {
+  if (query.category && query.category !== 'all') {
     filter.category = query.category;
   }
   if (query.name) {
